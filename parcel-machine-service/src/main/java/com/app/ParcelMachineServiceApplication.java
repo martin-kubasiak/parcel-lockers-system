67ae77f979ee6655cbdbe6cfb63f6c7f2ec8587b
MarcinK <marcinkubasiakdev@gmail.com>
2025-09-02 22:13:39 +0200

    feat: Initialize Parcel Machine Service microservice

---
+++ parcel-machine-service/src/main/java/com/app/ParcelMachineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcelMachineServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParcelMachineServiceApplication.class, args);
+    }
+
+}
