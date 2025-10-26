b4aa47b97a8e8f97e1f0148d28a252a8623fdb82
MarcinK <marcinkubasiakdev@gmail.com>
2025-10-26 22:01:41 +0100

    chore(user-service) update UserPreferences class

--- user-service/src/main/java/com/app/userservice/domain/model/UserPreferences.java
+++ user-service/src/main/java/com/app/userservice/domain/model/UserPreferences.java
@@ -1,6 +1,14 @@
 package com.app.userservice.domain.model;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserPreferences {
 
+    private final Map<String, UserPreferences> preferences = new HashMap<>();
 
+    Map<String, UserPreferences> getPreferences() {
+        return Collections.unmodifiableMap(preferences);
+    }
 }
