1dd3b0bb3216a06bc71922525efef2ce9eaf0e80
MarcinK <marcinkubasiakdev@gmail.com>
2025-12-26 00:43:17 +0100

    chore(parce-machine-service) add ParcelMachine entity in infrastructure layer

---
+++ parcel-machine-service/src/main/java/com/app/parcelmachineservice/infrastructure/output/persistence/entity/ParcelMachine.java
@@ -0,0 +1,46 @@
+package com.app.parcelmachineservice.infrastructure.output.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.locationtech.jts.geom.Point;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "parcel_machines")
+public class ParcelMachine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "osm_id", unique = true)
+    private Long osmId;
+
+    @Column(name = "overpass_ref")
+    private Long overpassRef;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "address_street")
+    private String addressStreet;
+
+    @Column(name = "address_city")
+    private String addressCity;
+
+    @Column(columnDefinition = "POINT SRID 4326", nullable = false)
+    private Point location;
+
+    @UpdateTimestamp
+    @Column(name = "last_updated", nullable = false)
+    private LocalDateTime lastUpdated;
+}
