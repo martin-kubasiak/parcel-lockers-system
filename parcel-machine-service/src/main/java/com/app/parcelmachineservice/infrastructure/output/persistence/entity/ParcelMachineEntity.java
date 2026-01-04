5712d283aba5c5ba2ed27a9358808e25d9f6a91e
MarcinK <marcinkubasiakdev@gmail.com>
2026-01-04 20:38:05 +0100

    chore(parcel-machine-service): implement equals and hashCode in ParcelMachineEntity, change overpassRef field type

--- parcel-machine-service/src/main/java/com/app/parcelmachineservice/infrastructure/output/persistence/entity/ParcelMachineEntity.java
+++ parcel-machine-service/src/main/java/com/app/parcelmachineservice/infrastructure/output/persistence/entity/ParcelMachineEntity.java
@@ -2,29 +2,32 @@ package com.app.parcelmachineservice.infrastructure.output.persistence.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
 import org.locationtech.jts.geom.Point;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @Entity
 @Table(name = "parcel_machines")
 public class ParcelMachineEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    private UUID id;
 
     @Column(name = "osm_id", unique = true)
     private Long osmId;
 
-    @Column(name = "overpass_ref")
-    private Long overpassRef;
+    @Column(name = "overpass_ref", unique = true)
+    private String overpassRef;
 
     @Column(nullable = false)
     private String name;
@@ -41,4 +44,27 @@ public class ParcelMachineEntity {
     @UpdateTimestamp
     @Column(name = "last_updated", nullable = false)
     private LocalDateTime lastUpdated;
+
+    @Builder
+    public ParcelMachineEntity(Long osmId, String overpassRef, String name,
+                               String addressStreet, String addressCity, Point location) {
+        this.osmId = osmId;
+        this.overpassRef = overpassRef;
+        this.name = name;
+        this.addressStreet = addressStreet;
+        this.addressCity = addressCity;
+        this.location = location;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ParcelMachineEntity that = (ParcelMachineEntity) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
